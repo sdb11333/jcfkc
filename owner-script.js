bfa917f0c1c07db587899d4113578ea3f7873c4f
sdb11333 <npripm682@gmail.com>
2025-03-08 21:52:47 +0300

    Update owner-script.js

--- owner-script.js
+++ owner-script.js
@@ -1,10 +1,25 @@
-// Код для панели основателя
 document.addEventListener('DOMContentLoaded', function() {
     const userRole = localStorage.getItem('userRole');
 
-    if (userRole !== 'owner') {
-        window.location.href = 'index.html'; // Если не основатель, перенаправляем на страницу входа
+    if (userRole !== 'admin') {
+        window.location.href = 'index.html'; // Если не админ, перенаправляем на страницу входа
     }
 
-    // Добавить здесь код для функционала панели основателя
+    // Функции для админ панели
+    function addUser() {
+        alert('Пользователь добавлен');
+    }
+
+    function deleteUser() {
+        alert('Пользователь удален');
+    }
+
+    function enable2FA() {
+        document.getElementById("2fa-status").innerText = "2FA: Включено";
+    }
+
+    function logout() {
+        localStorage.removeItem('userRole');
+        window.location.href = 'index.html'; // Перенаправление на страницу входа
+    }
 });
